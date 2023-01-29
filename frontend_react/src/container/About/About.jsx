2f24f4f266a00449835f0007dcdc438567fbd6bd
Iyinoluwa <iyinoyelz69@gmail.com>
2023-01-28 22:59:29 -0700

    adds sanity client for frontend

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,22 +1,30 @@
 import React, { useState, useEffect } from "react";
 
 import { motion } from "framer-motion";
+import { urlFor, client } from "../../client";
 
 import "./About.scss";
 
-const abouts = [
-  { title: "Fromtend", description: "I am a good web developer", imgUrl: "" },
-  { title: "Backend", description: "I am a good web developer", imgUrl: "" },
-  { title: "Full Stack", description: "I am a good web developer", imgUrl: "" },
-];
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      console.log(data)
+      setAbouts(data);
+    })
+    .catch((error) => {
+      console.log('err',error)
+    });
+  }, []);
+
   return (
     <>
       <h2 className="head-text">
-        I Know That <span>Good Design</span>
-        <br />
-        means
-        <span>Good Business</span>
+        I Know That <span>Good Dev</span>
+        <br /> means <span>Good Business</span>{" "}
       </h2>
       <div className="app__profiles">
         {abouts.map((about, i) => (
@@ -24,12 +32,16 @@ const About = () => {
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
             transition={{ duration: 0.5, type: "tween" }}
-            className='app__profile-item'
+            className="app__profile-item"
             key={about.title + i}
           >
-            <img src={about.imgUrl} alt={about.title} />
-            <h2 className="bold-text" style={{marginTop:20}}>{about.title}</h2>
-            <p className="bold-text" style={{marginTop:20}}>{about.description}</p>
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="bold-text" style={{ marginTop: 20 }}>
+              {about.description}
+            </p>
           </motion.div>
         ))}
       </div>
