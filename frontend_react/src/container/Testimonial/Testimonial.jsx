f4a9d3a636c1bf6f6feed411bd4adcb57baa059f
Iyinoluwa <iyinoyelz69@gmail.com>
2023-02-05 22:08:17 -0700

    adds testimonial setup

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
+
+import { motion } from "framer-motion";
+import { urlFor, client } from "../../client";
+
+
 import './Testimonial.scss'
 
 const Testimonial = () => {
+  const [brands, setBrands] = useState([]);
+  const [testimonial, setTestimonial] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const query = '*[_type == "testimonials"]';
+    const brandsQuery = '*[_type == "brands"]';
+
+    client.fetch(query).then((data) => {
+
+      setTestimonial(data);
+    });
+    client.fetch(brandsQuery).then((data) => {
+      setBrands(data)
+
+    });
+  }, []);
+
   return (
     <div>Testimonial</div>
   )
 }
 
-export default Testimonial
+export default AppWrap(
+  MotionWrap(Testimonial,'app__testimonial'),
+   "testimonial",
+   'app__primarybg'
+   );
