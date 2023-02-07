9067c063b24c46858fc6f98c0d2e8ebff6a4b700
Iyinoluwa <iyinoyelz69@gmail.com>
2023-02-06 23:56:37 -0700

    adds testimonial section

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
+import { AppWrap,MotionWrap } from "../../wrapper";
 
 import { motion } from "framer-motion";
 import { urlFor, client } from "../../client";
@@ -25,9 +26,38 @@ const Testimonial = () => {
 
     });
   }, []);
+  const test= testimonial[currentIndex]
 
+  const handleClick =(index) => {
+    setCurrentIndex(index);
+  }
   return (
-    <div>Testimonial</div>
+    <>
+    {testimonial.length && (
+      <>
+        <div className="app__testimonial-item app__flex">
+          <img src={urlFor(test.imgUrl)} alt="testimonial"className="" />
+          <div className="app__testimonial-content">
+            <p className="p-text">{test.feedback}</p>
+            <div className="bold-text">{test.name}</div>
+            <p className="bold-text">{test.company}</p>
+          </div>
+        </div>
+
+        <div className="app__testimonial-btns app__flex">
+          <div className="app__flex" onClick={()=> handleClick(currentIndex === 0 ? testimonial.length - 1  : currentIndex - 1 )}><HiChevronLeft /></div>
+          <div className="app__flex" onClick={()=> handleClick(currentIndex === testimonial.length - 1 ? 0  : currentIndex +1 )}><HiChevronLeft /></div>
+        </div>
+      </>
+    )}
+
+    <div className="app__testimonials-brands app__flex">{brands.map(brand => (
+      <motion.div whileInView={{opacity:[0,1]}} transition={{duration: 0.5, type:"tween"}}
+      key={brand._id}>
+        <img src={urlFor(brand.imgUrl)} alt={brand.name} />
+      </motion.div>
+    ))}</div>
+    </>
   )
 }
 
