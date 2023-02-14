1dd4ff46d26df5f85fa8b03392a5191bc308629c
Iyinoluwa <iyinoyelz69@gmail.com>
2023-02-13 23:57:30 -0700

    adds footer section contact form

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -1,13 +1,107 @@
-import React from 'react'
-import './Footer.scss'
+import React, { useState } from "react";
+import { images } from "../../constants";
+
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { client } from "../../client";
+
+import "./Footer.scss";
 
 const Footer = () => {
-  return (
-    <div className="copyright">
-    <p className="p-text">@2020 IYINOLUWA</p>
-    <p className="p-text">All rights reserved</p>
-  </div>
-  )
-}
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-export default Footer
+  const { name, email, message } = formData;
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target.value;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const handleSubmit = () => {
+    setLoading(true);
+
+    const contact ={
+      _type: 'contact',
+      name:name,
+      email:email,
+      message:message
+    }
+
+    client.create(contact)
+    .then(()=>{
+      setLoading(false);
+      setIsFormSubmitted(true);
+    })
+  }
+  return (
+    <>
+      <h2 className="head-text">Take a coffee and chat with me</h2>
+      <div className="app__footer-cards">
+        <div className="app__footer-card">
+          <img src={images.email} alt="email" />
+          <a href="mailto:iyinoyelz69@gmail.com" className="p-text">
+            iyinoyelz69@gmail.com
+          </a>
+        </div>
+        <div className="app__footer-card">
+          <img src={images.mobile} alt="mobile" />
+          <a href="tel:+1 (403) 404-3966" className="p-text">
+            ++1 (403) 404-3966
+          </a>
+        </div>
+      </div>
+      {!isFormSubmitted ?
+      <div className="app__footer-form app__flex">
+        <div className="app__flex">
+          <input
+            type="text"
+            className="p-text"
+            placeholder="Your Name"
+            name="name"
+            value={name}
+            onChange={handleChangeInput}
+          />
+        </div>
+        <div className="app__flex">
+          <input
+            type="text"
+            className="p-text"
+            placeholder="Your Email"
+            name="email"
+            value={email}
+            onChange={handleChangeInput}
+          />
+        </div>
+        <div>
+          <textarea
+            className="p-text"
+            placeholder="Your Message"
+            name="message"
+            value={message}
+            onChange={handleChangeInput}
+          ></textarea>
+        </div>
+        <button type="button" className="p-text" onClick={handleSubmit}>
+          {loading ? "Sending" : "Send Message"}
+        </button>
+      </div> :
+      <div>
+        <h3 className="head-text">
+          Thank you for getting in touch</h3>  
+      </div>}
+    </>
+  );
+};
+
+export default AppWrap(
+  MotionWrap(Footer, "app__footer"),
+  "contact",
+  "app__whitebg"
+);
